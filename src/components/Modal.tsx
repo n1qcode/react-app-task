4f8231da0aa6a6b8c13bd7d10cab45f10227e435
Nick <89103347+n1qcode@users.noreply.github.com>
2022-04-09 23:54:29 +0300

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,21 @@
 import React, {FC} from 'react';
 import {Card} from "antd";
 import LoginForm from "./LoginForm";
+import {useTypedSelector} from "../hooks/useTypedSelector";
 
 const Modal: FC = () => {
+    const { isModal } = useTypedSelector(state => state.auth);
     return (
-        <div className='modal_form_login'>
-            <div className='modal_form_login-body'>
-                <Card>
-                    <LoginForm />
-                </Card>
+        isModal ?
+            <div className='modal_form_login'>
+                <div className='modal_form_login-body'>
+                    <Card>
+                        <LoginForm />
+                    </Card>
+                </div>
             </div>
-        </div>
+                :
+                null
     );
 };
 
